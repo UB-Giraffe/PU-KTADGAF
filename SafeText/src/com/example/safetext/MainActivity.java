443a94907cff0f1f388e19167f0d4210a3576e16
atrookey <atrookey@buffalo.edu>
2013-11-09 09:01:10 -0500

    mylocation

--- SafeText/src/com/example/safetext/MainActivity.java
+++ SafeText/src/com/example/safetext/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.safetext;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	private GoogleMap mMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mMap.setMyLocationEnabled(true);
+
 	}
 
 	@Override
